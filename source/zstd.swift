131ab5b01ebb230c8c62181b6e017173b4e153d9
Hares <harryzjm@live.com>
2025-01-28 10:51:24 +0800

    update

--- source/zstd.swift
+++ source/zstd.swift
@@ -6,58 +6,83 @@ extension String: @retroactive Error {}
 public extension Data {
     // level between 0...ZSTD_MAX_CLEVEL
     func compressZstd(_ level: Int32 = ZSTD_CLEVEL_DEFAULT) throws -> Data {
-        try createZstdCompressedData(bytes: withUnsafeBytes { $0.baseAddress },
+        try Zstd.compressedData(bytes: withUnsafeBytes { $0.baseAddress },
                                      length: count,
                                      compressionLevel: level)
     }
     
     func decompressZstd() throws -> Data {
-        try createZstdDecompressedData(bytes: withUnsafeBytes { $0.baseAddress },
+        try Zstd.decompressedData(bytes: withUnsafeBytes { $0.baseAddress },
                                        length: count)
     }
 }
 
-
-private func createZstdCompressedData(bytes: UnsafeRawPointer?, length: Int, compressionLevel: Int32) throws -> Data {
-    guard let bytes = bytes, length > 0 else { throw "data is empty" }
-    
-    let maxOutputSize = ZSTD_compressBound(length)
-    
-    let outputBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: maxOutputSize)
-    defer { outputBuffer.deallocate() }
-    
-    let outputSize = ZSTD_compress(outputBuffer, maxOutputSize, bytes, length, compressionLevel)
-
-    guard ZSTD_isError(outputSize) == 0 else {
-        throw "output data is empty"
+struct Zstd {
+    static func compressed(path: String, toPath: String, level: Int32 = ZSTD_CLEVEL_DEFAULT) throws {
+        var isDirectory: ObjCBool = false
+        guard FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory) else {
+            throw "file not exist"
+        }
+        
+        if isDirectory.boolValue {
+            
+        }
     }
-
-    return Data(bytes: outputBuffer, count: outputSize)
+    
+    static func decompressed(path: String, toPath: String, level: Int32 = ZSTD_CLEVEL_DEFAULT) throws {
+        
+    }
 }
 
-private func createZstdDecompressedData(bytes: UnsafeRawPointer?, length: Int) throws -> Data {
-    guard let bytes = bytes, length > 0 else { throw "data is empty" }
-
-    let estimateOutputSize = ZSTD_getFrameContentSize(bytes, length)
-    if estimateOutputSize == ZSTD_CONTENTSIZE_ERROR {
-        throw  "not compressed by zstd"
-    }
-    if estimateOutputSize == ZSTD_CONTENTSIZE_UNKNOWN {
-        throw "original size unknown"
-    }
-
-    guard estimateOutputSize > 0 else {
-        throw "output data is empty"
+private extension Zstd {
+    static func compressedDirectory(path: String, toPath: String, level: Int32 = ZSTD_CLEVEL_DEFAULT) throws {
+        
     }
     
-    let outputBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(estimateOutputSize))
-    defer { outputBuffer.deallocate() }
-    
-    let outputSize = ZSTD_decompress(outputBuffer, Int(estimateOutputSize), bytes, length)
-
-    guard outputSize == estimateOutputSize else {
-        throw "Impossible because zstd will check this condition"
+    static func compressedFile(path: String, toPath: String, level: Int32 = ZSTD_CLEVEL_DEFAULT) throws {
     }
-
-    return Data(bytes: outputBuffer, count: Int(outputSize))
+    
+    static func compressedData(bytes: UnsafeRawPointer?, length: Int, compressionLevel: Int32) throws -> Data {
+        guard let bytes = bytes, length > 0 else { throw "data is empty" }
+        
+        let maxOutputSize = ZSTD_compressBound(length)
+        
+        let outputBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: maxOutputSize)
+        defer { outputBuffer.deallocate() }
+        
+        let outputSize = ZSTD_compress(outputBuffer, maxOutputSize, bytes, length, compressionLevel)
+        
+        guard ZSTD_isError(outputSize) == 0 else {
+            throw "output data is empty"
+        }
+        
+        return Data(bytes: outputBuffer, count: outputSize)
+    }
+    
+    static func decompressedData(bytes: UnsafeRawPointer?, length: Int) throws -> Data {
+        guard let bytes = bytes, length > 0 else { throw "data is empty" }
+        
+        let estimateOutputSize = ZSTD_getFrameContentSize(bytes, length)
+        if estimateOutputSize == ZSTD_CONTENTSIZE_ERROR {
+            throw  "not compressed by zstd"
+        }
+        if estimateOutputSize == ZSTD_CONTENTSIZE_UNKNOWN {
+            throw "original size unknown"
+        }
+        
+        guard estimateOutputSize > 0 else {
+            throw "output data is empty"
+        }
+        
+        let outputBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(estimateOutputSize))
+        defer { outputBuffer.deallocate() }
+        
+        let outputSize = ZSTD_decompress(outputBuffer, Int(estimateOutputSize), bytes, length)
+        
+        guard outputSize == estimateOutputSize else {
+            throw "Impossible because zstd will check this condition"
+        }
+        
+        return Data(bytes: outputBuffer, count: Int(outputSize))
+    }
 }
